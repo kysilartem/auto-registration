03c5bb676f82bd8387cf84c16a93402d054f8a97
artem.kysil <artem.kysil@quickblox.com>
2019-08-30 17:09:11 +0300

    output error6

--- src/test/java/com/Registration.java
+++ src/test/java/com/Registration.java
@@ -32,7 +32,11 @@ public class Registration{
         $(By.xpath("//*[@id=\"signup_terms\"]")).click();
         $(By.xpath("//*[@id=\"signup_submit\"]")).click();
 
-        System.out.println($(By.className("alert alert-danger ")).innerHtml());
+        System.out.println($(By.cssSelector("#wrap > div")).innerHtml());
+        System.out.println($(By.cssSelector("#wrap > div")).getSelectedText());
+        System.out.println($(By.cssSelector("#wrap > div")).text());
+        System.out.println($(By.cssSelector("#wrap > div")).innerText());
+        System.out.println($(By.cssSelector("#wrap > div")).getText());
     }
 
 
