66f3ad0afc5f1c0bdf32133c00816b2ee048a3e6
artem.kysil <artem.kysil@quickblox.com>
2019-09-03 11:49:01 +0300

    final test5

--- src/test/java/com/Registration.java
+++ src/test/java/com/Registration.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 
 import java.io.IOException;
 
+import static com.codeborne.selenide.Selectors.byXpath;
 import static com.codeborne.selenide.Selenide.*;
 
 public class Registration{
@@ -19,18 +20,18 @@ public class Registration{
         Credentials.initCredentials();
 
         open(Credentials.getUrl());
-        $(By.xpath("//*[@id=\"login-block\"]/div/div/div/p/a")).click();
-        $(By.xpath("//*[@id=\"reg-block\"]/div/div/div/h3")).shouldBe(Condition.visible);
+        $(byXpath("//*[@id=\"login-block\"]/div/div/div/p/a")).click();
+        $(byXpath("//*[@id=\"reg-block\"]/div/div/div/h3")).shouldBe(Condition.visible);
 
-        $(By.xpath("//*[@id=\"user_full_name\"]")).setValue(Credentials.getFullName());
-        $(By.xpath("//*[@id=\"user_email\"]")).setValue(Credentials.getEmail());
-        $(By.xpath("//*[@id=\"user_login\"]")).setValue(Credentials.getLogin());
-        $(By.xpath("//*[@id=\"user_password\"]")).setValue(Credentials.getPassword());
-        $(By.xpath("//*[@id=\"user_password_confirmation\"]")).setValue(Credentials.getPassword());
-        $(By.xpath("//*[@id=\"user_registration_code\"]")).setValue(Credentials.getRegCode());
-        $(By.xpath("//*[@id=\"signup_terms\"]")).click();
+        $(byXpath("//*[@id=\"user_full_name\"]")).setValue(Credentials.getFullName());
+        $(byXpath("//*[@id=\"user_email\"]")).setValue(Credentials.getEmail());
+        $(byXpath("//*[@id=\"user_login\"]")).setValue(Credentials.getLogin());
+        $(byXpath("//*[@id=\"user_password\"]")).setValue(Credentials.getPassword());
+        $(byXpath("//*[@id=\"user_password_confirmation\"]")).setValue(Credentials.getPassword());
+        $(byXpath("//*[@id=\"user_registration_code\"]")).setValue(Credentials.getRegCode());
+        $(byXpath("//*[@id=\"signup_terms\"]")).click();
         Thread.sleep(1500);
-        $(By.xpath("//*[@id=\"signup_submit\"]")).click();
+        $(byXpath("//*[@id=\"signup_submit\"]")).click();
 
     }
 
